108bf44a512012446c70d027470740402fcba618
Negima <paolo.demarchi@usach.cl>
2023-03-23 10:30:37 -0300

    Base data
    Base data and organization of the project

---
+++ src/main/java/tintin/tingeso2023/Tingeso2023Application.java
@@ -0,0 +1,13 @@
+package tintin.tingeso2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tingeso2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tingeso2023Application.class, args);
+	}
+
+}
